007fdd8dde1fefdb89bda6a41a49851637b6de32
Yorrick Jansen <yorrick.jansen@1science.org>
2016-03-07 14:24:03 -0500

    Type classes adapters

---
+++ src/main/scala/yorrick/designpatterns/typeclasses.scala
@@ -0,0 +1,31 @@
+package yorrick.designpatterns
+
+import annotation.implicitNotFound
+
+
+object Math {
+  
+  @implicitNotFound("No member of type class NumberLike in scope for ${T}")
+  trait NumberLike[T] {
+    def plus(x: T, y: T): T
+    def divide(x: T, y: Int): T
+    def minus(x: T, y: T): T
+  }
+  
+  object NumberLike {
+    implicit object NumberLikeDouble extends NumberLike[Double] {
+      def plus(x: Double, y: Double): Double = x + y
+      def divide(x: Double, y: Int): Double = x / y
+      def minus(x: Double, y: Double): Double = x - y
+    }
+  }
+}
+
+
+object Statistics {
+  import Math.NumberLike
+  
+  def mean[T](xs: Vector[T])(implicit ev: NumberLike[T]): T =
+    ev.divide(xs.reduce(ev.plus(_, _)), xs.size)
+
+}
---
+++ src/test/scala/yorrick/designpatterns/TypeClassesTest.scala
@@ -0,0 +1,25 @@
+package yorrick.designpatterns
+
+import org.scalatest.{FlatSpec, Matchers, OptionValues, TryValues}
+import Statistics._
+import Math._
+
+
+class TypeClassesTest extends FlatSpec with Matchers with OptionValues with TryValues {
+
+  "type classes" should "use defined type classes" in {
+    val numbers = Vector[Double](13, 23.0, 42, 45, 61, 73, 96, 100, 199, 420, 900, 3839)
+    mean(numbers) shouldBe 484.25
+  }
+
+  "type classes" should "allow to extend API by defining new type classes" in {
+    implicit object NumberLikeInt extends NumberLike[Int] {
+      def plus(x: Int, y: Int): Int = x + y
+      def divide(x: Int, y: Int): Int = x / y
+      def minus(x: Int, y: Int): Int = x - y
+    }
+
+    val numbers = Vector[Int](1, 3, 5, 8, 4)
+    mean(numbers) shouldBe 4  // value stay an integer (by API design)
+  }
+}
