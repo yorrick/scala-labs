6e89ff13c6c805aa1deb47bffe461c990f78c1ae
Yorrick Jansen <yorrick.jansen@1science.org>
2016-03-23 11:56:11 -0400

    More examples

--- src/main/scala/yorrick/functionalprogramming/Exercice5_2.scala
+++ src/main/scala/yorrick/functionalprogramming/Exercice5_2.scala
@@ -79,6 +79,7 @@ trait Stream[+A] {
   def filter(p: A => Boolean): Stream[A] = foldRight(empty[A])((h, acc) => if (p(h)) cons(h, acc) else acc)
   def append[B >: A](s: => Stream[B]): Stream[B] = foldRight(s)((h, acc) => cons(h, acc))
   def flatMap[B >: A](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((h, acc) => f(h).append(acc))
+  def find(p: A => Boolean): Option[A] = filter(p).headOption
 }
 
 case object Empty extends Stream[Nothing]
@@ -96,6 +97,32 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+  
+  val ones: Stream[Int] = cons(1, ones)
+  
+  // This is more efficient than `cons(a, constant(a))` since it's just one object referencing itself
+  def constant[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+  }
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fibs: Stream[Int] = {
+    def go(prev: Int, cur: Int): Stream[Int] = {
+      cons(prev, go(cur, prev + cur))
+    }
+    go(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case None => empty[A]
+    case Some((a, newState)) => cons(a, unfold(newState)(f))
+  }
+
+  def fibsWithUnfold: Stream[Int] = unfold((0, 1)) { case (prev: Int, cur: Int) => Some((prev, (cur, prev + cur))) }
+  def constantWithUnfold[A](a: A): Stream[A] = unfold(a)(_ => Some((a, a)))
+  def fromWithUnfold(n: Int): Stream[Int] = unfold(n)(s => Some((s, s + 1)))
 }
 
 
@@ -132,5 +159,15 @@ object Test {
     
     println(Stream(1, 2).append(Stream(3, 4)).toList)
     println(Stream(1, 2).flatMap(i => Stream(i, i)).toList)
+    
+    println(ones.take(4).toList)
+    println(ones.map(_ + 1).exists(_ % 2 == 0))
+    println(ones.forAll(_ != 1))
+    
+    println(from(4).take(4).toList)
+    println(fibs.take(7).toList)
+    println(fibsWithUnfold.take(7).toList)
+    println(constantWithUnfold(1).take(7).toList)
+    println(fromWithUnfold(4).take(7).toList)
   }
 }
