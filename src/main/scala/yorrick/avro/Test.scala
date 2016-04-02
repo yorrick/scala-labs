b8f0f8baf397c99bbdc2711cf2f0c5623f3e0b04
Yorrick Jansen <yorrick.jansen@1science.org>
2016-04-01 22:16:44 -0400

    Avro examples

--- src/main/scala/yorrick/avro/Test.scala
+++ src/main/scala/yorrick/avro/Test.scala
@@ -1,12 +1,21 @@
 package yorrick.avro
 
+import java.io.ByteArrayOutputStream
+
 import com.gensler.scalavro.types.AvroType
 import spray.json.JsValue
 
 
 object Test {
-  def toAvroUser(user: User): JsValue = {
+  def toJson(user: User): JsValue = {
     val userType = AvroType[User]
     userType.io writeJson user
+  }
+
+  def toBinary(user: User): Array[Byte] = {
+    val userType = AvroType[User]
+    val os = new ByteArrayOutputStream()
+    userType.io.write(user, os)
+    os.toByteArray
   }
 }
--- src/test/scala/yorrick/avro/AvroTest.scala
+++ src/test/scala/yorrick/avro/AvroTest.scala
@@ -9,7 +9,11 @@ class AvroTest extends FlatSpec with Matchers with OptionValues with TryValues {
 
   val jsonUser = """{"name":"name","favoriteNumber":{"int":2},"favoriteColor":null}"""
   
-  "avro user" should "return user" in {
-    toAvroUser(User("name", Some(2), None)) shouldBe JsonParser(jsonUser)
+  "user" should "be serializable in json" in {
+    toJson(User("name", Some(2), None)) shouldBe JsonParser(jsonUser)
+  }
+  
+  "user" should "be serializable in binary" in {
+    toBinary(User("name", Some(2), None)) shouldBe Array[Byte](8, 110, 97, 109, 101, 2, 4, 0)
   }
 }
