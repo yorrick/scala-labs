8d923faa614589947ad7d56eec3614689d0cc475
Yorrick Jansen <yorrick.jansen@1science.org>
2016-11-22 11:02:41 -0500

    Make function tail rec

--- src/main/scala/yorrick/yarnlogs/CollectionImplicits.scala
+++ src/main/scala/yorrick/yarnlogs/CollectionImplicits.scala
@@ -1,17 +1,22 @@
 package yorrick.yarnlogs
 
+import scala.annotation.tailrec
+
 object CollectionImplicits {
   implicit class RichList[T](xs: List[T]) {
     /** Returns shortest possible list of lists xss such that
      *   - xss.flatten == xs
      *   - No sublist in xss contains an element matching p in its tail
-     *  TODO make that tail recursive!
      */
-    def groupPrefix(p: T => Boolean): List[List[T]] = xs match {
-      case List() => List()
-      case x :: xs1 =>
-        val (ys, zs) = xs1 span (!p(_))
-        (x :: ys) :: zs.groupPrefix(p)
+    def groupPrefix(p: T => Boolean): List[List[T]] = groupPrefixTailRec(List.empty)(xs)(p)
+  }
+
+  @tailrec
+  private def groupPrefixTailRec[T](acc: List[List[T]])(xs: List[T])(p: T => Boolean): List[List[T]] = xs match {
+    case x :: tail => {
+      val (ys, zs) = tail span (!p(_))
+      groupPrefixTailRec(acc :+ (x :: ys))(zs)(p)
     }
+    case List() => acc
   }
 }
