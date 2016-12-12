904ec328fc60c6ebaf8b770876facf6749251f79
Yorrick Jansen <yorrick.jansen@1science.org>
2016-12-12 16:44:01 -0500

    Added tests with Applicative

---
+++ src/main/scala/yorrick/learningscalaz/ApplicativeTest.scala
@@ -0,0 +1,25 @@
+package yorrick.learningscalaz
+
+import scalaz.Applicative
+import scalaz.Scalaz._
+
+
+object ApplicativeTest {
+  def main(args: Array[String]): Unit = {
+    // Applicative type
+    println(1.point[List])
+
+    println(9.some <*> {(_: Int) + 3}.some)
+
+    println(List(1, 2, 3) <*> List((_: Int) * 0, (_: Int) + 100, (x: Int) => x * x))
+
+    println(sequenceA(List(1.some, 2.some)))
+    println(sequenceA(List.empty[Option[Int]]))
+  }
+
+  // transforms a list of applicative into an applicative of list
+  def sequenceA[F[_]: Applicative, A](list: List[F[A]]): F[List[A]] = list match {
+    case Nil     => List.empty[A].point[F]  // builds new instance with empty list
+    case x :: xs => (x |@| sequenceA(xs)) apply {_ :: _}  // maps Applicative and adds list head
+  }
+}
