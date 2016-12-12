6da8551929605ee441cfbcab5d75c076f4dfe6d1
Yorrick Jansen <yorrick.jansen@1science.org>
2016-12-12 14:52:28 -0500

    Added more examples with CanTruthy

--- src/main/scala/yorrick/learningscalaz/CanTruthy.scala
+++ src/main/scala/yorrick/learningscalaz/CanTruthy.scala
@@ -40,13 +40,31 @@ object ToCanIsTruthyOps {
 
 object CanTruthyTest {
   def main(args: Array[String]): Unit = {
-    implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
+    implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys {
       case 0 => false
       case _ => true
-    })
+    }
 
     println(10.truthy)
 
+    implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys {
+      case Nil => false
+      case _ => true
+    }
 
+    implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.truthys(_ => false)
+
+    println(List("foo").truthy)
+    println(Nil.truthy)
+
+    implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
+
+    println(truthyIf (Nil) {"YEAH!"} {"NO!"})
+    println(truthyIf (2 :: 3 :: 4 :: Nil) {"YEAH!"} {"NO!"})
+    println(truthyIf (true) {"YEAH!"} {"NO!"})
   }
+
+  def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
+    if (cond.truthy) ifyes
+    else ifno
 }
