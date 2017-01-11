33184ae18f05d3de6ebffed55f83a7200bcfc3ca
Yorrick Jansen <yorrick.jansen@1science.org>
2017-01-11 15:49:27 -0500

    Added tests on Monoids

---
+++ src/test/scala/yorrick/learningscalaz/MonoidTest.scala
@@ -0,0 +1,39 @@
+package yorrick.learningscalaz
+
+import org.scalatest.{FlatSpec, Matchers, OptionValues, TryValues}
+import scalaz._
+import Scalaz._
+
+import scalaz.{Monoid => SZMonoid}
+
+
+class MonoidTest extends FlatSpec with Matchers with OptionValues with TryValues {
+  "scalaz monoids" should "define tags to choose between monoids" in {
+    // simplest case: default Monoid[Int] represents addition
+    SZMonoid[Int].zero shouldBe 0
+    10 |+| SZMonoid[Int].zero shouldBe 10
+
+    // we can choose among monoids bt using Tags
+    SZMonoid[Int @@ Tags.Multiplication].zero shouldBe 1
+    Tags.Multiplication(10) |+| SZMonoid[Int @@ Tags.Multiplication].zero shouldBe 10
+  }
+
+  "scalaz Ordering monoid" should "allow for multiple levels of comparison" in {
+    def lengthCompare(lhs: String, rhs: String): Ordering = (lhs.length ?|? rhs.length) |+| (lhs ?|? rhs)
+
+    lengthCompare("zen", "ants") shouldBe Ordering.LT
+    lengthCompare("zen", "ant") shouldBe Ordering.GT
+
+    case class Person(age: Int, name: String, other: Int = 0)
+
+    def personCompare(p1: Person, p2: Person): Ordering =
+      (p1.age ?|? p2.age) |+| (p1.name ?|? p2.name) |+| (p1.hashCode() ?|? p2.hashCode())
+
+    personCompare(Person(10, "toto"), Person(12, "toto")) shouldBe Ordering.LT
+    personCompare(Person(13, "toto"), Person(12, "toto")) shouldBe Ordering.GT
+
+    personCompare(Person(12, "tota"), Person(12, "toto")) shouldBe Ordering.LT
+    personCompare(Person(12, "toto"), Person(12, "toto")) shouldBe Ordering.EQ
+    personCompare(Person(12, "toto", 2), Person(12, "toto")) shouldBe Ordering.GT
+  }
+}
